175b6bd50c119e320c2da7f646ce485f79dc8f6c
Jake Kampf <jkampf@sundevils.asu.edu>
2019-03-28 17:23:24 -0700

    Added post class.

---
+++ Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USAADemoApp
+{
+    class Post
+    {
+        private int voteRank;
+        private string postTitle;
+        private string postContent;
+
+        public int VoteRank { get => voteRank; set => voteRank = value; }
+        public string PostTitle { get => postTitle; set => postTitle = value; }
+        public string PostContent { get => postContent; set => postContent = value; }
+    }
+}

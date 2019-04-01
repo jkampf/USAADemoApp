901bdd40858f1464022961a5c192b3b67636574a
Jake Kampf <jkampf@sundevils.asu.edu>
2019-04-01 16:46:57 -0700

    Refined GUI

--- PostBoard.Designer.cs
+++ PostBoard.Designer.cs
@@ -31,14 +31,18 @@
             this.listBoxPosts = new System.Windows.Forms.ListBox();
             this.comboBoxPostCategory = new System.Windows.Forms.ComboBox();
             this.labelPostCategory = new System.Windows.Forms.Label();
+            this.buttonNewPost = new System.Windows.Forms.Button();
+            this.checkBoxIsManager = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // listBoxPosts
             // 
             this.listBoxPosts.FormattingEnabled = true;
-            this.listBoxPosts.Location = new System.Drawing.Point(15, 67);
+            this.listBoxPosts.ItemHeight = 16;
+            this.listBoxPosts.Location = new System.Drawing.Point(20, 82);
+            this.listBoxPosts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBoxPosts.Name = "listBoxPosts";
-            this.listBoxPosts.Size = new System.Drawing.Size(397, 329);
+            this.listBoxPosts.Size = new System.Drawing.Size(528, 404);
             this.listBoxPosts.TabIndex = 2;
             // 
             // comboBoxPostCategory
@@ -49,28 +53,52 @@
             "Implementation",
             "Issues",
             "Suggestions"});
-            this.comboBoxPostCategory.Location = new System.Drawing.Point(94, 25);
+            this.comboBoxPostCategory.Location = new System.Drawing.Point(125, 31);
+            this.comboBoxPostCategory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxPostCategory.Name = "comboBoxPostCategory";
-            this.comboBoxPostCategory.Size = new System.Drawing.Size(143, 21);
+            this.comboBoxPostCategory.Size = new System.Drawing.Size(266, 24);
             this.comboBoxPostCategory.TabIndex = 1;
             // 
             // labelPostCategory
             // 
             this.labelPostCategory.AutoSize = true;
-            this.labelPostCategory.Location = new System.Drawing.Point(12, 25);
+            this.labelPostCategory.Location = new System.Drawing.Point(16, 31);
+            this.labelPostCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPostCategory.Name = "labelPostCategory";
-            this.labelPostCategory.Size = new System.Drawing.Size(76, 13);
+            this.labelPostCategory.Size = new System.Drawing.Size(101, 17);
             this.labelPostCategory.TabIndex = 3;
             this.labelPostCategory.Text = "Post Category:";
             // 
+            // buttonNewPost
+            // 
+            this.buttonNewPost.Location = new System.Drawing.Point(241, 517);
+            this.buttonNewPost.Name = "buttonNewPost";
+            this.buttonNewPost.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewPost.TabIndex = 4;
+            this.buttonNewPost.Text = "New Post";
+            this.buttonNewPost.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIsManager
+            // 
+            this.checkBoxIsManager.AutoSize = true;
+            this.checkBoxIsManager.Location = new System.Drawing.Point(454, 34);
+            this.checkBoxIsManager.Name = "checkBoxIsManager";
+            this.checkBoxIsManager.Size = new System.Drawing.Size(94, 21);
+            this.checkBoxIsManager.TabIndex = 5;
+            this.checkBoxIsManager.Text = "Manager?";
+            this.checkBoxIsManager.UseVisualStyleBackColor = true;
+            // 
             // PostBoard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.ClientSize = new System.Drawing.Size(579, 569);
+            this.Controls.Add(this.checkBoxIsManager);
+            this.Controls.Add(this.buttonNewPost);
             this.Controls.Add(this.labelPostCategory);
             this.Controls.Add(this.comboBoxPostCategory);
             this.Controls.Add(this.listBoxPosts);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PostBoard";
             this.Text = "Post Board";
             this.ResumeLayout(false);
@@ -83,5 +111,7 @@
         private System.Windows.Forms.ListBox listBoxPosts;
         private System.Windows.Forms.ComboBox comboBoxPostCategory;
         private System.Windows.Forms.Label labelPostCategory;
+        private System.Windows.Forms.Button buttonNewPost;
+        private System.Windows.Forms.CheckBox checkBoxIsManager;
     }
 }

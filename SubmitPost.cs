4dd613ede501330cee798a1961dd5408c4e91732
Russell Downer <russelldowner321@gmail.com>
2019-04-07 10:06:27 -0700

    database implementation

--- SubmitPost.cs
+++ SubmitPost.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 using static System.String;
 namespace USAADemoApp
 {
@@ -18,14 +24,60 @@ namespace USAADemoApp
             if (radioButtonImplementation.Checked == true)
             {
                 postCategory = 1;
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "scrumdevils.cis440.com";
+                builder.Database = "scrumdevils_";
+                builder.UserID = "scrumdevils";
+                builder.Password = "!!Cis440";
+
+                MySqlConnection connection = new MySqlConnection(builder.ToString());
+                connection.Open();
+
+                string baseq = "INSERT INTO `usaa`(`Ranking`, `post title`, `post body`, `feedback type`) VALUES (2,'value-2','value-3','value-4')";
+
+
+                string insertImplementation = "INSERT into `usaa` (`Ranking`, `post title`, `post body`,`feedback type`) VALUES ('4','" + textBoxPostTitle.Text + "','" + textBoxPostBody.Text + "'," + "'implementation');";
+                MySqlCommand insertImplementationCommand = new MySqlCommand(insertImplementation, connection);
+                insertImplementationCommand.ExecuteNonQuery();
+
             }
             else if (radioButtonIssues.Checked == true)
             {
                 postCategory = 2;
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "scrumdevils.cis440.com";
+                builder.Database = "scrumdevils_";
+                builder.UserID = "scrumdevils";
+                builder.Password = "!!Cis440";
+
+                MySqlConnection connection = new MySqlConnection(builder.ToString());
+                connection.Open();
+
+                string baseq = "INSERT INTO `usaa`(`Ranking`, `post title`, `post body`, `feedback type`) VALUES (2,'value-2','value-3','value-4')";
+
+
+                string insertImplementation = "INSERT into `usaa` (`Ranking`, `post title`, `post body`,`feedback type`) VALUES ('4','" + textBoxPostTitle.Text + "','" + textBoxPostBody.Text + "'," + "'Issues');";
+                MySqlCommand insertImplementationCommand = new MySqlCommand(insertImplementation, connection);
+                insertImplementationCommand.ExecuteNonQuery();
             }
             else if (radioButtonSuggestions.Checked == true)
             {
                 postCategory = 3;
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "scrumdevils.cis440.com";
+                builder.Database = "scrumdevils_";
+                builder.UserID = "scrumdevils";
+                builder.Password = "!!Cis440";
+
+                MySqlConnection connection = new MySqlConnection(builder.ToString());
+                connection.Open();
+
+                string baseq = "INSERT INTO `usaa`(`Ranking`, `post title`, `post body`, `feedback type`) VALUES (2,'value-2','value-3','value-4')";
+
+
+                string insertImplementation = "INSERT into `usaa` (`Ranking`, `post title`, `post body`,`feedback type`) VALUES ('4','" + textBoxPostTitle.Text + "','" + textBoxPostBody.Text + "'," + "'suggestions');";
+                MySqlCommand insertImplementationCommand = new MySqlCommand(insertImplementation, connection);
+                insertImplementationCommand.ExecuteNonQuery();
             }
 
             if (textBoxPostTitle.Text != Empty && textBoxPostBody.Text != Empty && postCategory != 0)

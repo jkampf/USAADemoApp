ef99fed5a8ff8bbb5d939bc46f19a65cad672f2a
Jake Kampf <jkampf@sundevils.asu.edu>
2019-04-12 12:30:06 -0700

    Fixed uparrow issue.

--- PostBoard.cs
+++ PostBoard.cs
@@ -189,6 +189,9 @@ namespace USAADemoApp
             listBoxIssues.DataSource = listOfIssuePosts;
             listOfSuggestionPosts = new BindingList<Post>();
             listBoxSuggestions.DataSource = listOfSuggestionPosts;
+            pictureBoxUpvote.Hide();
+            pictureBoxDownvote.Hide();
+            labelCount.Hide();
         }
 
         private void CheckBoxIsManager_CheckedChanged(object sender, System.EventArgs e)
